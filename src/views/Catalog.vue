cc1aea013ff5123859f2e460519da189d7a38e85
Roman <76100057+RomanMorf@users.noreply.github.com>
2021-09-17 12:08:04 +0300

    создали карточку товара

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -1,14 +1,35 @@
 <template>
   <div>
     <h1 class="center">This is an catalog page</h1>
+    <div class="card-wrapper">
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+      <Card></Card>
+    </div>
   </div>
 </template>
 
 <script>
+import Card from '@/components/Catalog/Card'
 
 export default {
   name: 'Catalog',
   components: {
+    Card,
   }
 }
 </script>
+
+<style lang="scss">
+  .card-wrapper {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-wrap: wrap;
+  }
+</style>
