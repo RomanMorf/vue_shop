b94401eca49042d06192946d0e218cd2cf93cd45
Роман Костромичов <romankostromichov@MacBook-Air-Romanuk.local>
2021-09-24 11:04:56 +0300

    login form

---
+++ src/views/Login.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="container">
+    <div class="form">
+      <h3>Вход в личный кабинет</h3>
+      <form @click.prevent="">
+        <div class="form_section">
+          <p>Email</p>
+          <input v-model="userEmail" type="email" placeholder="Enter your email">
+        </div>
+        <div class="form_section">
+          <p>Password</p>
+          <input v-model="userPass" type="password" placeholder="Enter your password">
+        </div>
+        <div class="form_section btn">
+          <button>Войти</button>
+          <a href="#">Восстановить пароль</a>
+          <a href="#">Зарегитрироваться</a>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login',
+  data() {
+    return {
+      userEmail: '',
+      userPass: '',
+    }
+  },
+}
+</script>
+
+<style lang="scss">
+  .login {
+    &_form {
+      display: flex;
+      flex-direction: row;
+    }
+
+    &_section {
+      display: flex;
+    }
+
+  }
+</style>
