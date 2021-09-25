96ebcac51421cceb4242fd7d03fc619ea9083540
Kostromichov Andrej <kostromichov.andrej@gmail.com>
2021-09-25 12:04:52 +0300

    save

---
+++ src/components/CategoryBlock/CategoryBlock.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="category">
+    <div class="content">
+      <h2>Название категории</h2>
+      <p>Текст блока на главной 1</p>
+      <div class="btn">Смотреть</div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped lang="scss">
+  @import 'CategoryBlock.scss';
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,30 @@
 <template>
   <div class="home">
     <h1 class="center">This is an home page</h1>
-
+    <Loader></Loader>
+    <div class="section">
+      <CategoryBlock></CategoryBlock>
+      <CategoryBlock></CategoryBlock>
+      <CategoryBlock></CategoryBlock>
+    </div>
   </div>
 </template>
 
 <script>
-
+import CategoryBlock from '@/components/CategoryBlock/CategoryBlock.vue'
 export default {
   name: 'Home',
   components: {
+    CategoryBlock,
   }
 }
 </script>
+
+<style scoped lang="scss">
+  .section {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+  }
+</style>
