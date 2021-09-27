0476a087af228689bb7d822035a8e995ceb921fa
Kostromichov Andrej <kostromichov.andrej@gmail.com>
2021-09-27 18:37:07 +0300

    CategoryBlock

--- src/components/CategoryBlock/CategoryBlock.vue
+++ src/components/CategoryBlock/CategoryBlock.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="category">
-    <div class="content">
-      <h2>Название категории</h2>
-      <p>Текст блока на главной 1</p>
-      <div class="btn">Смотреть</div>
-    </div>
+    <img src="https://static-sl.insales.ru/images/products/1/1891/300566371/large_13258025-1-black.jpeg" alt="">
+    <h2 class="h2">Название категории</h2>
+    <p>Текст блока на главной 1</p>
+    <span>Смотреть</span>
   </div>
-  
 </template>
 
 <script>
